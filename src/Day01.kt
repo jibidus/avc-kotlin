4f456da70bc90f4ed272b65ce6b4934c2e138777
Jibidus <jibidus@gmail.com>
2023-12-09 11:14:30 +0100

    Day01 - part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { Pair(it.firstDigit(), it.lastDigit()) }.sumOf { "${it.first}${it.second}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +9,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
+    "Answer is…".println()
     part1(input).println()
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun String.firstDigit() = find { it.isDigit() }
+
+fun String.lastDigit() = findLast { it.isDigit() }
